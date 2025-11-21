2ebb7482a321c016e3b40da1cafda58dbe68de04
Alvin Koh <alvinkoh@AlvinKoh-MBP-Q4NM9GRWYK.local>
2025-11-21 10:10:01 +0800

    added table borders, fixed cell width

--- app/components/toDoForm.tsx
+++ app/components/toDoForm.tsx
@@ -5,14 +5,14 @@ import { toast } from "react-toastify";
 
 export default function TodoForm() {
     const [title, setTitle] = useState("")
-    const [priority, setPriority] = useState("low")
+    const [priority, setPriority] = useState("Low")
     return (
         <div>
             <input type="text" className="bg-white text-red-600 rounded-lg m-2 h-8" value={title} onChange={(e) => setTitle(e.target.value)} />
             <select className="bg-white m-2 text-black rounded-lg h-8" value={priority} onChange={(e) => setPriority(e.target.value)}>
-                <option value="low">Low</option>
-                <option value="med">Medium</option>
-                <option value="high">High</option>
+                <option value="Low">Low</option>
+                <option value="Medium">Medium</option>
+                <option value="High">High</option>
             </select>
 
             <button className="cursor-pointer bg-teal-500 rounded-sm pl-3 pr-3 pt-1 pb-1 m-2 hover:bg-violet-600 active:bg-blue-500" onClick={
@@ -20,7 +20,7 @@ export default function TodoForm() {
                     if (title.trim() !== "") {
                         addTodo(title, priority);
                         setTitle("");
-                        toast.success('Added new task: ' + title + ' with ' + ' priority.')
+                        toast.success('Added new task: ' + title + ' - ' + priority + ' priority.')
                     } else {
                         toast.error('You need to write something!');
                     }
--- app/components/todoList.tsx
+++ app/components/todoList.tsx
@@ -8,38 +8,40 @@ import { toast } from 'react-toastify';
 export default function TodoList({ todos }: { todos: Todo[] }) {
     let content;
     if (todos.length === 0) {
-        content = <p className="text-gray-400">No Todos yet</p>;
+        content = <tr><td className="col-span-2"><p className="text-gray-400">No Todos yet</p></td></tr>;
     } else {
         (content =
-            <table className="">
+            todos.map((todoEach: Todo) => (
+                <tr key={todoEach.id}>
+                    <td>
+                        <p>{todoEach.title}</p>
+                    </td>
+                    <td>
+                        <p>{todoEach.priority}</p>
+                    </td>
+                    <td>
+                        <button className="bg-blue-600 cursor-pointer m-2 pl-2 pr-2 pt-1 pb-1 rounded-xs" onClick={
+                            () => {
+                                deleteTodo(todoEach.id);
+                                toast.success('Deleted task: ' + todoEach.title);
+                            }
+                        }>Delete</button>
+                    </td>
+                </tr>
+            ))
+        );
+    }
+    return (
+        <div className="overflow-hidden rounded-sm">
+            <table className="todo-table border-1 border-collapse">
                 <tr>
                     <th>Task</th>
                     <th>Priority</th>
                     <th></th>
                 </tr>
-                {todos.map((todoEach: Todo) => (
-                    <tr key={todoEach.id}>
-                        <td>
-                            <p>{todoEach.title}</p>
-                        </td>
-                        <td>
-                            <p>{todoEach.priority}</p>
-                        </td>
-                        <td>
-                            <button className="bg-blue-600 cursor-pointer m-2 pl-2 pr-2 pt-1 pb-1 rounded-xs" onClick={
-                                () => {
-                                    deleteTodo(todoEach.id);
-                                    toast.success('Deleted task: ' + todoEach.title);
-                                }
-                            }>Delete</button>
-                        </td>
-                    </tr>
-                ))}
-            </table>);
-    }
-    return (
-        <div>
-            {content}
+                {content}
+            </table>
+
         </div>
     )
 }
